83a564ff0a966ae777f54bad0f6ea8141250dd0f
Ore-456 <orianstar6@gmail.com>
2025-05-21 13:32:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.getElementById('year').textContent = new Date().getFullYear();
+ document.getElementById('year').textContent = new Date().getFullYear();
 
 const quotes = [
-    "Keep going, you’re doing great!",
-    "You’re capable of amazing things 💪🏽",
-    "Small steps lead to big change.",
-    "You got this, seriously!"
-  ];
+  "Hire me and Hire well ;)",
+  "You’re capable of amazing things 💪🏽",
+  "Small steps lead to big change.",
+  "You got this, seriously!"
+];
 
-  function showQuote() {
-    const random = Math.floor(Math.random() * quotes.length);
-    document.getElementById("quote").innerText = quotes[random];
-  }
+function showQuote() {
+  const random = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quote").innerText = quotes[random];
+}
